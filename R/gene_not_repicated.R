514db928283de3cfb64e831fe9dd44c3e59447cc
Maxime Bonjean <m.bonjean@student.uclouvain.be>
2020-07-10 11:14:21 +0200

    bon repertoire

--- R/gene_not_repicated.R
+++ R/gene_not_repicated.R
@@ -7,7 +7,7 @@ gene_not_duplicated = function(species,geneDir,outDir,collicin)
     dir.create(paste0(outDir,species[i]))
       for (j in j:ngene) {
         fasta.list <- readDNAStringSet(paste0(geneDir,species[i],"/",collicin[j],".fasta"))
-        writeXStringSet(unique(fasta.list),paste0(outDir,collicin[j]))
+        writeXStringSet(unique(fasta.list),paste0(outDir,species[i],"/",collicin[j],".fasta"))
       }
   }
 }
