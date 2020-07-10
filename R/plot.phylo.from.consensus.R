948ca5f2c1e36c50a4fa62b9b489f8dc385c08ab
JeromeAmbroise <jerome.ambroise@uclouvain.be>
2020-07-10 09:50:19 +0200

    add phylum information in phylogenetic tree

--- R/plot.phylo.from.consensus.R
+++ R/plot.phylo.from.consensus.R
@@ -6,20 +6,39 @@
 #' @export
 #'
 #' @examples
-phylo.from.consensus <- function(consensusDir)
+phylo.from.consensus <- function(consensusDir,bacteria.table)
 {
-  filelist <- list.files(consensusDir,full.names = T)
-
+  library(dplyr)
   library(Biostrings)
   library(muscle)
   library(phangorn)
   library(ggtree)
+
+  filelist <- list.files(consensusDir,full.names = T)
+
+  correspondance.organism.subgroup <- bacteria.table%>%group_by(Organism) %>% dplyr::count(Organism, SubGroup) %>% dplyr::slice(which.max(n)) %>% dplyr::rename(species=Organism)
+
+
 
   genename <- gsub(basename(filelist),pattern = '.fasta',replacement = '')
   ngenes <- length(genename)
   for(i in 1:ngenes)
   {
     sequences <- readDNAStringSet(filelist[i])
+
+    sequences <- sequences[sort.list(names(sequences))]
+    currentname <- names(sequences)
+    newname <- currentname
+    newname.part1 <- paste(unlist(lapply(strsplit(newname,split=' '),function(x) x[[1]])),unlist(lapply(strsplit(newname,split=' '),function(x) x[[2]])),sep='_')
+
+    newname.part2 <- unlist(lapply(strsplit(newname,split=' '),function(x) x[[3]]))
+    newname.part2 <- data.frame(species=newname.part2)
+    newname.part2 <- left_join(newname.part2,correspondance.organism.subgroup,by='species')
+    newname.part2 <- paste(newname.part2$SubGroup,newname.part2$species,sep='_')
+    newname <- paste(newname.part1,newname.part2,sep='_')
+
+    names(sequences) <- newname
+
     align.muscle <- muscle::muscle(sequences)
     dist1 <- stringDist(as(align.muscle,"DNAStringSet"), method="hamming")
     mytree1 <- upgma(dist1)
