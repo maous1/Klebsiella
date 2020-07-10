87a97aecc5c609ec520a78d9ba21352253842144
JeromeAmbroise <jerome.ambroise@uclouvain.be>
2020-07-10 12:12:30 +0200

    adaptation of gene not duplicated function

--- R/gene_not_repicated.R
+++ R/gene_not_repicated.R
@@ -1,13 +1,42 @@
+#' Title A function to exclude replicated genes within a species
+#'
+#' @param species
+#' @param geneDir
+#' @param outDir
+#' @param collicin
+#'
+#' @return
+#' @export
+#'
+#' @examples
 gene_not_duplicated = function(species,geneDir,outDir,collicin)
 {
+
+  library(Biostrings)
   collicin <- collicin$genename
   ngenes <- length(collicin)
   nspecies=length(species)
-  for (i in i:nspecies) {
+  for (i in 1:nspecies) {
     dir.create(paste0(outDir,species[i]))
-      for (j in j:ngene) {
-        fasta.list <- readDNAStringSet(paste0(geneDir,species[i],"/",collicin[j],".fasta"))
-        writeXStringSet(unique(fasta.list),paste0(outDir,species[i],"/",collicin[j],".fasta"))
-      }
+    for (j in 1:ngenes) {
+      fasta.list <- readDNAStringSet(paste0(geneDir,species[i],"/",collicin[j],".fasta"))
+      writeXStringSet(unique(fasta.list),paste0(outDir,species[i],"/",collicin[j],".fasta"))
+    }
   }
+
+
+  fasta.list=c()
+  for (i in 1:nspecies) {
+    fasta.list.newspecies <- list.files(paste0(outDir,species[i],'/'),full.names = T)
+    fasta.list.newspecies <- fasta.list.newspecies[grep('fasta',fasta.list.newspecies)]
+    fasta.list <- c(fasta.list,fasta.list.newspecies)
+  }
+
+  genename <- unique(basename(fasta.list))
+  for(i in 1:length(genename))
+  {
+    current.sequence <- readDNAStringSet(fasta.list[grep(genename[i],fasta.list)])
+    writeXStringSet(current.sequence,paste0(outDir,genename[i]))
+  }
+
 }
